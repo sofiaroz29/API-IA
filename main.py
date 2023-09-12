21c923a4d0d5c8d69cb76f7d6620335e4db3dd8c
sofiaroz29 <101563492+sofiaroz29@users.noreply.github.com>
2023-09-12 16:36:58 -0300

    .

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Form, UploadFile, File
+import base64
+from  predecir import predecir_imagen
+from PIL import Image
+import urllib.request
+
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return "Hi im fastapi"
+
+
+@app.post("/upload")
+def result(image_url:  str = Form(...)):
+    
+    try:
+        resultado = predecir_imagen(image_url)
+        porcentaje = round(resultado * 100, 2);
+        print(resultado)
+        print(porcentaje);
+         
+    except Exception as e:
+        print(e)
+        return {"message": "Hubo un error al subir la img"}
+
+    return str(porcentaje)
---
+++ predecir.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from PIL import Image
+import requests
+
+
+
+altura, longitud = 100, 100
+model = tf.keras.models.load_model('model.h5')
+
+
+
+def predecir_imagen(ruta_imagen):
+   img = Image.open(requests.get(ruta_imagen, stream = True).raw).resize((120, 120))
+   image_array = tf.keras.preprocessing.image.img_to_array(img) / 255.0
+   image_array = np.expand_dims(image_array, axis=0)
+
+   prediction = model.predict(image_array)[0][0]
+   #probabilidades = prediccion[0] * 100
+   label = "melanoma" if prediction >= 0.5 else "no melanoma"
+
+   print("The probability of melanoma is:", prediction)
+   print("Prediction:", label)
+   return prediction
